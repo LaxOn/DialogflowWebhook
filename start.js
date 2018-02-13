f8a113547dce481cc1af8376eefa4305888dbe45
Laxon <br_lacson@yahoo.com>
2018-02-13 10:27:01 -0500

    testing 2

--- start.js
+++ start.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  port = process.env.PORT || 5000,
   bodyParser = require('body-parser');
   
 
@@ -11,17 +11,18 @@ app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
 
-let routes = function(app) {
-	let controller = require('./controller.js');
 
-  // todoList Routes
-	app.route('/action')
-		.post(controller.postImage)
-};
-
-routes(app);
 
 let postImage = function(req, res){
 	console.log(req);
 	console.log(res);
 }
+
+let routes = function(app) {
+
+  // todoList Routes
+	app.route('/action')
+		.post(postImage);
+};
+
+routes(app);
